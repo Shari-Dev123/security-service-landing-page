32d0afb7c2fb151cd4267ce791f3eef125727e36
afaankhawaja <aafaan02@gmail.com>
2026-02-19 22:01:31 +0500

    updated the arrow for about and service page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,8 +11,23 @@ const AboutPage = () => {
             <Navbar />
             {/* <div className="pt-20 h-[100vh]"> */}
             {/* <AboutUs /> */}
-            <About />
-            <ContactSection />
+            <div className='relative'>
+                <About />
+                <div className='absolute -bottom-25 z-100 left-[50%] translate-x-[-50%]'>
+                    <svg width="100" height="100" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 0 H50 L25 25 Z" fill="white" />
+                    </svg>
+                </div>
+            </div>
+            <div className='relative'>
+                <ContactSection />
+                <div className='absolute -bottom-25 z-100 left-[50%] translate-x-[-50%]'>
+                    <svg width="100" height="100" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 0 H50 L25 25 Z" fill="white" />
+                    </svg>
+                </div>
+            </div>
+            
             {/* </div> */}
             <Footer about={true} />
             <Chatbot />
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -7,7 +7,14 @@ const page = () => {
   return (
     <div className='min-h-screen relative'>
       <Navbar />
-      <ServicesGrid />
+      <div className='relative'>
+                <ServicesGrid />
+                <div className='absolute -bottom-17.8 z-100 left-[50%] translate-x-[-50%]'>
+                    <svg width="100" height="100" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 0 H50 L25 25 Z" fill="white" />
+                    </svg>
+                </div>
+            </div>
       <Footer services={true} />
       <Chatbot />
     </div>
