d21826482234d6a1d7227937b8d1c729c83ecedc
afaankhawaja <aafaan02@gmail.com>
2026-02-25 20:53:09 +0500

    updated the blue line on ipad

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       
       <div className='relative'>
         <Excellence />
-        <div className='absolute -bottom-[99px] z-100 left-[50%] translate-x-[-50%]'>
+        <div className='absolute -bottom-[98px] z-100 left-[50%] translate-x-[-50%]'>
           <svg width="100" height="100" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
             <path d="M0 0 H50 L25 25 Z" fill="white" />
           </svg>
