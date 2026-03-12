dce07e7f572a553157dca1555f97a963391ac000
Shari-Dev123 <freelancingwithsharishah@gmail.com>
2026-03-13 00:29:01 +0500

    imges and pages issuse solve

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,30 @@
+// // import type { NextConfig } from "next";
+
+// // const nextConfig: NextConfig = {
+// //   /* config options here */
+// // };
+
+
+// // export default nextConfig;
+
 // import type { NextConfig } from "next";
 
+// // Next.js config
 // const nextConfig: NextConfig = {
-//   /* config options here */
+//   // agar pehle kuch options hain, yahan add karo
+//   output: "export", // ye line static export enable karti hai
 // };
-
 
 // export default nextConfig;
 
-import type { NextConfig } from "next";
+// next.config.ts
+import { NextConfig } from 'next';
 
-// Next.js config
 const nextConfig: NextConfig = {
-  // agar pehle kuch options hain, yahan add karo
-  output: "export", // ye line static export enable karti hai
+  output: 'export', // you already have this
+  images: {
+    unoptimized: true, // add this line
+  },
 };
 
 export default nextConfig;
